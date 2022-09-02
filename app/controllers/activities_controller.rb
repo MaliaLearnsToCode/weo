7f9ee85f9fe4c98e7a24213dc774f9e46c1a919b
Felita Liem <felital2020@gmail.com>
2022-09-02 18:47:48 +0800

    omg this took so long

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [ :show ]
+  before_action :set_activity, only: %i[show success]
+  before_action :set_itinerary, only: %i[new create]
 
   def index
     @activities = Activity.all
@@ -12,13 +13,38 @@ class ActivitiesController < ApplicationController
   end
 
   def new
+    @activity = Activity.new
+    @user = @itinerary.user
+    authorize @activity
   end
 
   def create
+    # debugger
+    @activity = Activity.new(activity_params)
+    @activity.itinerary = @itinerary
+
+    authorize @activity
+
+    # if @activity.save
+    #   redirect_to itinerary_path(@itinerary)
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+
+    respond_to do |format|
+      if @activity.save
+        format.html { redirect_to itinerary_path(@itinerary) }
+      else
+        format.html { render "activities/new", status: :unprocessable_entity }
+      end
+      format.json
+    end
+
   end
 
+
+
   def show
-    @activity = Activity.find(params[:id])
     authorize @activity
   end
 
@@ -30,4 +56,19 @@ class ActivitiesController < ApplicationController
 
   def pending
   end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :detail, :location, :max_participants, :completed, :start_date, :end_date, :itinerary_id, :start_time, :end_time, :interest_type_id, photo: [])
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def set_itinerary
+    @itinerary = Itinerary.find(params[:itinerary_id])
+  end
+
 end
--- app/policies/activity_policy.rb
+++ app/policies/activity_policy.rb
@@ -1,11 +1,17 @@
 class ActivityPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-    def show?
-      true
+    def resolve
+      scope.all
     end
+
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
   end
 end
