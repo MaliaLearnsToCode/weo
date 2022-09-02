d0276c2af63e9d695c671505451993dfa79e20a8
Danish Hisham <mddanishhisham@gmail.com>
2022-09-02 14:04:23 +0800

    Tried to if else statment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # Check if user is signed in
+  # If user IS signed in then skip this page and route to itineraries index
+  # If user is NOT signed in, then show this page
+  skip_before_action :authenticate_user!, only: [:home]
+  def check_sign
+
+  end
+
+  if user_signed_in?
+    puts
+  else
+    puts "user is not"
+  end
+  # link_to /itineraries
 
   def home
   end
